f59fb7bf56b09f0ef83dd70993ef21925f91a2cf
nomis <xianghuan.kong@outlook.com>
2026-02-05 21:08:20 +0800

    init project

---
+++ apps/movie-app/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import './global.css';
+
+export const metadata = {
+  title: 'Movie App',
+  description: '',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ apps/movie-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.tailwind file.
+   */
+  return (
+    <div>
+    </div>
+  );
+}
